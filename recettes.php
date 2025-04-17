21e7e731c5509ed632c09cfb224d60d1c3bdbd52
gearpimento <antoine.haugazeau@efrei.net>
2025-04-17 20:48:12 +0200

    Add files via upload
    page table des recettes dans la bdd ainsi que une page de présentation

---
+++ recettes.php
@@ -0,0 +1,53 @@
+<?php 
+$title ="Page des recettes";
+include '../inc/header.php'; ?>
+
+    <main>
+        <h1>Nos recettes!</h1>
+        <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nom de la recette</th>
+                    <th>Catégorie</th>
+                    <th>Description</th>
+                    <th>Liste des ingrédients</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                include '../inc/connexionlogin.php';
+                $req = 'SELECT recettes.*, categories.nom AS nom_categorie FROM recettes JOIN categories ON recettes. id_categorie = categories.id_categorie';
+                $resultat = $connexion ->query($req);
+                foreach ($resultat AS $i):
+                ?>
+                <tr>
+                    <td><?= $i['nom'] ?></td>
+                    <td><?= $i['nom_categorie']?></td>
+                    <td><?= $i['description']?></td>
+                    <td>
+                    <?php 
+                        $id_recette = $i['id_recette'];
+                        $req = "SELECT ingredients.nom FROM recette_ingredient 
+                                JOIN ingredients ON recette_ingredient.id_ingredient = ingredients.id_ingredient 
+                                WHERE recette_ingredient.id_recette = $id_recette";
+                        $res = $connexion->query($req);
+
+                        $liste = [];
+                        foreach ($res as $ing) {
+                            $liste[] = $ing['nom'];
+                        }
+                    
+                        echo implode(', ', $liste);
+                    ?>
+
+                    </td>
+                </tr>
+                <?php endforeach;?>
+            </tbody>
+
+        </table>
+        </div>
+    </main>
+
+<?php include '../inc/footer.php';
